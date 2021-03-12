8b4f5652858ee39d60442500b3ab0e59f350a0c1
Fabiana Genila Ramos Pereira <fabianagenila@gmail.com>
2021-03-12 13:39:01 -0300

    código para o projeto Whatslab, não funciona

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React from 'react'
 import './App.css';
+import styled from 'styled-components'
+import { Whatslab } from './components/Whatslab';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// const ContainerDoApp = styled.div`
+//   display: flex;
+//   flex: 1;
+//   flex-direction: column;
+//   max-width: 600px;
+//   height: 100vh;
+//   border: 1px solid black;
+// `;
+
+// const ContainerDaMensagem = styled.div`
+//   display: flex;
+//   flex: 1;
+//   flex-direction: column;
+//   padding: 20px;
+//   justify-content: flex-end;
+// `
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      mensagens: []
+    }
+  }
+
+  adicionaMensagem = (mensagem) => {
+    this.setState({mensagens: [...this.state.mensagens, mensagem]})
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.mensagens.map((mensagem, index) => <p key={index}><span>{mensagem.usuario}</span>{': ' + mensagem.texto}</p>)}
+        </div>
+        <form adicionaMensagem={this.adicionaMensagem} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
+
+
+
