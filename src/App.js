3fe35cbac378443f116815848ed92446826d0338
Nicolas <nicolasalexandre0001@gmail.com>
2021-03-12 13:24:00 -0300

    estilização

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import Mensagem from './components/Mensagem.js'
 
 function App() {
   return (
-    <div>
+    <div style={{backgroundColor:'gainsboro', width:'1000px', display:'flex', flexDirection:'column'}}>
       <Mensagem autor='Nicolas' texto='Está é mensagem!'/>
+      <Mensagem autor='Eu' texto='E está é a minha mensagem!'/>
+      <Mensagem autor='Lorem' texto='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis mauris vitae dui laoreet sodales. Proin feugiat a libero et porta. Praesent id auctor nibh, sit amet cursus massa. Maecenas pretium eget nisi vitae malesuada. Proin tincidunt sodales velit, at commodo arcu. Donec ut justo consequat diam tempus sagittis vitae sit amet justo.'/>
+      <Mensagem autor='eu' texto='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis iaculis mauris vitae dui laoreet sodales. Proin feugiat a libero et porta. Praesent id auctor nibh, sit amet cursus massa. Maecenas pretium eget nisi vitae malesuada. Proin tincidunt sodales velit, at commodo arcu. Donec ut justo consequat diam tempus sagittis vitae sit amet justo.'/>
+
     </div>
   );
 }
--- src/components/Mensagem.js
+++ src/components/Mensagem.js
@@ -1,43 +1,57 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
-  align-items: flex-start
-`
+  align-items: flex-start;
+  margin: 5px;
+`;
 
 const Mensagem = styled.div`
-  display:flex;
-  background-color: #EEE;
-  width: 800px;
+  display: flex;
+  background-color: #DDF7C8;
+  max-width: 600px;
   min-height: 4em;
   border-radius: 8px;
-  box-shadow: 5px 4px 3px gray;
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
   flex-direction: column;
-  padding:5px;
-  justify-content:space-around;
-`
+  padding: 10px;
+  justify-content: space-around;
+`;
 const Autor = styled.div`
+  color: #9AAC8C;
+  font-size: 13px;
+  text-transform: capitalize;
   font-weight: bold;
-`
+`;
 const Avatar = styled.img`
   border-radius: 50%;
   width: 50px;
   margin: 10px;
-`
-
-
+`;
 
 export default class extends React.Component {
-  render(){
-    return (
-      <Container>
-      <Avatar src='https://www.flaticon.com/svg/vstatic/svg/149/149071.svg?token=exp=1615562533~hmac=a4d4a746381e3addff9a98d97147198c'/>
-      <Mensagem>
-        <Autor>{this.props.autor}</Autor>
-        {this.props.texto}
-      </Mensagem>
-      </Container>
-    )
+  render() {
+    const mensagem = () => {
+      if (this.props.autor.toLowerCase() === "eu") {
+        return (
+          <Container style={{ alignSelf: "flex-end" }}>
+            <Mensagem>{this.props.texto}</Mensagem>
+            <Avatar src="https://www.flaticon.com/svg/vstatic/svg/149/149071.svg?token=exp=1615562533~hmac=a4d4a746381e3addff9a98d97147198c" />
+          </Container>
+        );
+      } else {
+        return (
+          <Container>
+            <Avatar src="https://www.flaticon.com/svg/vstatic/svg/149/149071.svg?token=exp=1615562533~hmac=a4d4a746381e3addff9a98d97147198c" />
+            <Mensagem style={{backgroundColor:'white'}}>
+              <Autor>{this.props.autor}</Autor>
+              {this.props.texto}
+            </Mensagem>
+          </Container>
+        );
+      }
+    };
+    return <>{mensagem()}</>;
   }
 }
